1cde9f2a1c44dd719079f1cc665cff9d948e6c04
elior <DarknerY_037131>
2022-05-11 22:21:02 +0300

    Логическое отрицание ! [#334072]

--- src/main/java/ru/job4j/condition/LogicNot.java
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -2,27 +2,27 @@ package ru.job4j.condition;
 
 public class LogicNot {
 
-    public static boolean isEven(int num) { //    isEven() - проверяет, что число четное;
+    public static boolean isEven(int num) {
         return num % 2 == 0;
     }
 
-    public static boolean isPositive(int num) { //        2. isPositive() -  проверяет, что число положительное.
+    public static boolean isPositive(int num) {
         return num > 0;
     }
 
-    public static boolean notEven(int num) { //        - метод notEven(), проверяет, что число нечетное;
+    public static boolean notEven(int num) {
         return !isEven(num);
     }
 
-    public static boolean notPositive(int num) { //        - метод notPositive(), проверяет, что число не положительное;
+    public static boolean notPositive(int num) {
         return !isPositive(num);
     }
 
-    public static boolean notEvenAndPositive(int num) { //- метод notEvenAndPositive(), проверяет, что число нечетное и положительное;
+    public static boolean notEvenAndPositive(int num) {
         return notEven(num) && isPositive(num);
     }
 
-    public static boolean evenOrNotPositive(int num) { //        - метод evenOrNotPositive(), проверяет, что число четное или не положительное.
+    public static boolean evenOrNotPositive(int num) {
         return isEven(num) || notPositive(num);
     }
 
@@ -38,16 +38,3 @@ public class LogicNot {
 
 }
 
-//
-
-//
-//        Вам необходимо дописать код в остальные методы и при этом использовать уже реализованные методы в:
-//
-
-//
-
-//
-//        а также сочетания реализованных методов в указанных ниже методах:
-//
-//
-//
