0660a6e5f4bde343237b3717fb47f34971f0e8d7
elior <DarknerY_037131>
2022-05-19 00:23:47 +0300

    Простое число [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = false;
+            for (int i = 1; i <= number; i++) {
+                if (number % i != 0 && number % 2 != 0) {
+                    prime = true;
+                }
+            }
+        return prime;
+    }
+}
