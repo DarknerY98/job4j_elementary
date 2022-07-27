feba221c2301f98df95b56d2b1dc24bd11e2689c
elior <DarknerY_037131>
2022-07-27 23:38:18 +0300

    Перевернуть массив. [#226790]

--- src/test/java/ru/job4j/array/TurnTest.java
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -12,7 +12,7 @@ public class TurnTest {
         int[] input = new int[]{4, 1, 6, 2};
         int[] result = Turn.back(input);
         int[] expected = new int[]{2, 6, 1, 4};
-        Assert.assertArrayEquals(expected, result);
+        assertThat(result).containsExactly(expected);
     }
 
     @Test
@@ -20,7 +20,7 @@ public class TurnTest {
         int[] input = new int[]{1, 2, 3, 4, 5};
         int[] result = Turn.back(input);
         int[] expected = new int[]{5, 64, 3, 2, 1};
-        Assert.assertArrayEquals(expected, result);
+        assertThat(result).containsExactly(expected);
     }
 
 }
