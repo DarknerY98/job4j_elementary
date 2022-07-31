896fe273dec8c7f677f0cbeb186a2fd8265b2465
elior <DarknerY_037131>
2022-08-01 01:02:00 +0300

    Перевернуть массив. [#226790]

--- src/test/java/ru/job4j/array/TurnTest.java
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TurnTest {
 
@@ -12,7 +13,7 @@ public class TurnTest {
         int[] input = new int[]{4, 1, 6, 2};
         int[] result = Turn.back(input);
         int[] expected = new int[]{2, 6, 1, 4};
-        Assert.assertArrayEquals(expected, result);
+        assertThat(result).containsExactly(expected);
     }
 
     @Test
@@ -20,7 +21,7 @@ public class TurnTest {
         int[] input = new int[]{1, 2, 3, 4, 5};
         int[] result = Turn.back(input);
         int[] expected = new int[]{5, 4, 3, 2, 1};
-        Assert.assertArrayEquals(expected, result);
+        assertThat(result).containsExactly(expected);
     }
 
 }
