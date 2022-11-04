47c28064b465d431bcc4a3cc0d19cbff6754135e
elior <DarknerY_037131>
2022-11-04 21:58:21 +0300

    Жадный алгоритм. Сдача в кофе машине [#34741]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,12 @@
 package ru.job4j;
 
+import ru.job4j.array.Matrix;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+        System.out.println(Matrix.multiple(2));
+
     }
 }
