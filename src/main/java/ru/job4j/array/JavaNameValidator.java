6d3125d6f85c085c7de1d3e74b0aa035d04b6c13
elior <DarknerY_037131>
2022-12-05 22:07:30 +0300

    String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,51 @@
+package ru.job4j.array;
+
+import static java.lang.Character.isDigit;
+import static java.lang.Character.isUpperCase;
+
+public class JavaNameValidator {
+    @SuppressWarnings("checkstyle:SimplifyBooleanExpression")
+    public static boolean isNameValid(String name) {
+        boolean valid = false;
+        if (!name.isEmpty()) {
+                if (isLowerLatinLetter(name.codePointAt(0))) {
+                    if (!isDigit(name.codePointAt(0))) {
+                        for (int i = 0; i < name.length(); i++) {
+                            int code = name.codePointAt(i);
+                            if (isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code)) {
+                                valid = true;
+                                break;
+                    }
+                  }
+                }
+            }
+        }
+        return valid;
+
+    }
+
+        public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 90;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        boolean valid = false;
+        if (code >= 65 && code <= 90) {
+            valid = true;
+        }
+        return valid;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        boolean valid = false;
+        if (code >= 97  && code <= 122) {
+            valid = true;
+        }
+        return valid;
+
+    }
+
+    public static void main(String[] args) {
+       System.out.println(isNameValid("first"));
+    }
+}
