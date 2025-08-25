efbfc3a03d5f409095a45c396f5113f232bd3de9
DarknerY98 <darknery95@gmail.com>
2025-08-25 22:04:07 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -1,8 +1,6 @@
 package ru.job4j.array;
 
 public class SortSelected {
-
-
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length - 1; i++) {
             int minIndex = i;
@@ -12,7 +10,6 @@ public class SortSelected {
                     minIndex = j;
                 }
             }
-
 
             if (minIndex != i) {
                 int temp = data[i];
